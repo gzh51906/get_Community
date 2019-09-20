a0c1ee90837c2dcf4c880acd281e31612739bbb2
黄日隆 <l>
2019-09-20 14:11:38 +0800

    完成头部菜单大概样式

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,141 @@
 import React from "react";
 import {Route,Redirect,NavLink,Switch} from "react-router-dom";
+import {withRouter} from 'react-router';
+import { Layout,Drawer, Button,Menu, Icon } from 'antd';
+
+
+//黄日隆
+import Login from './route/hrl/login';
+import './App.css';
+const { Header, Footer, Sider, Content } = Layout;
+const { SubMenu } = Menu;
 
 class App extends React.Component{
+    rootSubmenuKeys = ['sub1', 'sub2','sub3', 'sub4'];
+
+    //数据
+    state = {
+     visible: false,
+     openKeys: ['sub4'],
+    }
+    //方法
+     showDrawer = () => {
+         this.setState({
+             visible: true,
+         });
+     };
+
+     onClose = () => {
+         this.setState({
+             visible: false,
+         });
+     };
+
+     //菜单的方法
+     onOpenChange = openKeys => {
+         const latestOpenKey = openKeys.find(key => this.state.openKeys.indexOf(key) === -1);
+         if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+             this.setState({
+                 openKeys
+             });
+         } else {
+             this.setState({
+                 openKeys: latestOpenKey ? [latestOpenKey] : [],
+             });
+         }
+     };
+
+
     render(){
         return <div style={{height:"100%"}}>
-            陈日兴、 黄日隆、 苏沛龙、 崔宇灏小组
+            <Layout>
+                <Header style={{backgroundColor:"#fff",height:'1.08rem'}}>
+                    
+                    <Button onClick={this.showDrawer}>
+                        <Icon type="menu"></Icon>
+                        </Button>
+                        <Drawer
+                        placement="left"
+                        closable={false}
+                        onClose={this.onClose}
+                        visible={this.state.visible}
+                        >
+                        <Layout>
+                            <Header style={{height:'3.6rem',backgroundColor:'#ccc',padding:0}}>
+                                <Button style={{float:'left',marginTop:'1.6rem',marginLeft:'0.8rem'}}>注册</Button>
+                                <Button type="primary" style={{float:'right',marginTop:'1.6rem',marginRight:'0.8rem'}}>登录</Button>
+                            </Header>
+                            <Content>
+                                 <Menu
+                                mode="inline"
+                                openKeys={this.state.openKeys}
+                                onOpenChange={this.onOpenChange}
+                                style={{ width: '5.12rem'}}
+                            >
+                                <SubMenu
+                                key="sub1"
+                                title={
+                                    <span>
+                                    <Icon type="home" theme='filled'/>
+                                    <span>首页</span>
+                                    </span>
+                                }
+                                >
+                                </SubMenu>
+                                <SubMenu
+                                key="sub2"
+                                title={
+                                    <span>
+                                    <Icon type="profile"/>
+                                    <span>资讯</span>
+                                    </span>
+                                }
+                                >
+                                </SubMenu>
+                                 <SubMenu
+                                key="sub3"
+                                title={
+                                    <span>
+                                    <Icon type="shopping" theme="filled"/>
+                                    <span>扫货</span>
+                                    </span>
+                                }
+                                >
+                                </SubMenu>
+                                <SubMenu
+                                key="sub4"
+                                title={
+                                    <span>
+                                    <Icon type="notification" theme="filled"/>
+                                    <span>社区</span>
+                                    </span>
+                                }
+                                >
+                                <Menu.Item key="9">评测区</Menu.Item>
+                                <Menu.Item key="10">晒图区</Menu.Item>
+                                <Menu.Item key="11">运动品牌鉴定区</Menu.Item>
+                                <Menu.Item key="12">潮牌鉴定区</Menu.Item>
+                                </SubMenu>
+                            </Menu>
+                            </Content>
+                            <Footer style={{textAlign:"center"}}>退出账号</Footer>
+                        </Layout>
+                    </Drawer>
+                </Header>
+                <Content>Content
+                    
+                    {/* 使用路由-黄日隆 */}
+                    <Route path="/login" component={Login}></Route>
+                    
+                </Content>
+            </Layout>
+
+
+
+
+
+                
+                
         </div>
     }
 }
---
+++ client/src/route/hrl/login.js
@@ -0,0 +1,29 @@
+//引入
+import React,{Component} from 'react';
+import {
+    Form,
+    Input,
+    Tooltip,
+    Icon,
+    Cascader,
+    Select,
+    Row,
+    Col,
+    Checkbox,
+    Button,
+    AutoComplete,
+} from 'antd';
+import {Route,NavLink,Switch} from 'react-router-dom';
+
+class Login extends Component{
+    render(){
+        return(
+            <div>登录注册页面
+
+               
+            </div>
+        )
+    }
+}
+
+export default Login;
